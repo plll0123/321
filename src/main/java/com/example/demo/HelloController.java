e139f8f0107bc106946b1e2aac197c525a08bfb8
pll0123 <102746073+pll0123@users.noreply.github.com>
2022-07-12 01:44:18 +0900

    add new service.class

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,13 +1,20 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello() {
+    HelloService service;
+    @RequestMapping
+    public String hello123() {
         return "helloworld";
+    }
+
+    @RequestMapping
+    public String controllerhello() {
+        return service.newHello() + hello123();
     }
 }
